8fe7477a8e4af4db6f8e410e898c362c50b4dd32
107leo <pranavmore20121998@gmail.com>
2021-11-27 21:29:01 -0600

    fixed radio button

--- signup/views.py
+++ signup/views.py
@@ -23,7 +23,7 @@ def signupView(request):
         street = str(request.POST.get("street"))
         zip = str(request.POST.get("zip"))
         password = str(request.POST.get("password"))
-
+        usertype = str(request.POST.get("usertype"))
         #inserting new user into users table
         insertEmailQuery = "Insert into users(username) values('" + email + "');"
         errorMsg = "Cannot insert user into db"        
@@ -37,13 +37,18 @@ def signupView(request):
         id = str(row[0][0])
 
         #insert password and client type in login table
-        insertPasswordQuery = "Insert into login(id, password, type) values(" + id + ", '"+ password + "', 'client');"
+        insertPasswordQuery = "Insert into login(id, password, type) values(" + id + ", '"+ password + "', '" + usertype + "');"
         insertPassword = db.insertOrUpdateOrDelete(insertPasswordQuery, errorMsg)
 
-        insertClientQuery = "Insert into client values(" + id + ", '"+ firstName +"', '"+ lastName +"', '"+ state +"', '"+ city +"', '"+ street +"', " + zip + ", " + phoneNumber + ", " + cellNumber + ", 'client');"
-        insertClient = db.insertOrUpdateOrDelete(insertClientQuery, errorMsg)
+        insertClientQuery = "Insert into client values(" + id + ", '"+ firstName +"', '"+ lastName +"', '"+ state +"', '"+ city +"', '"+ street +"', " + zip + ", " + phoneNumber + ", " + cellNumber + ", 'silver');"
+        insertTraderQuery = "Insert into trader values(" + id + ", '"+ firstName +"', '"+ lastName +"', '"+ state +"', '"+ city +"', '"+ street +"', " + zip + ", " + phoneNumber + ", " + cellNumber + ");"
+        
+        if usertype == "client":
+            insertClient = db.insertOrUpdateOrDelete(insertClientQuery, errorMsg)
+        elif usertype == "trader":
+            insertTrader = db.insertOrUpdateOrDelete(insertTraderQuery, errorMsg)
 
-        if insertEmail and insertPassword and insertClient:
+        if insertEmail and insertPassword and (insertTrader or insertClient):
             return redirect('login')
         
     return render(request, 'signup.html', context)
